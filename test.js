714ff74c414f2d2ac10cde876c74c390d21375ee
manojpodugu <psmk126@gmail.com>
2026-03-10 21:18:58 +0530

    Update test output message for CI success

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const sum = require('./app');
 
 if (sum(2,3) === 5) {
-  console.log("Test passed");
+  console.log("CI Test Passed Successfully");
   process.exit(0);
 } else {
   console.log("Test failed");
